a553efb5a7e1da9addd58ca48b77edfeab535097
Nikita <fedosovnikita007@gmail.com>
2024-12-23 20:43:57 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 1000, // Указываем порт
+    strictPort: true, // Если порт 1000 занят, сервер не запустится
+    host: true, // Позволяет доступ к серверу из локальной сети (например, с других устройств)
+  },
   plugins: [
     vue(),
     vueDevTools(),
